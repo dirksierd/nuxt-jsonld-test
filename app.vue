d00f746d204d4dc5069da9bd16f71c0ef84bbf1c
Dirk Sierd de Vries <dirksierd@me.com>
2024-09-13 12:02:13 +0200

    more data in json-ld

--- app.vue
+++ app.vue
@@ -12,6 +12,19 @@
         type: 'application/ld+json',
         innerHTML: {
           '@context': 'http://schema.org',
+          '@graph': [
+            {
+              '@context': 'https://schema.org',
+              '@type': 'Article',
+              author: {
+                '@type': 'PoliticalParty',
+                name: 'Party 123',
+                url: 'https://party.gov',
+              },
+              headline: 'Headline',
+              datePublished: '2024-09-13T10:01:57.065Z',
+            }
+          ]
         }
       }
     ]
