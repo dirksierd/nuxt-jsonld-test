372a0db3d0952e76fd9325be04c9ca0aa7c56729
Dirk Sierd de Vries <dirksierd@me.com>
2024-09-13 12:02:53 +0200

    added workaround for cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { env, nodeless, cloudflare } from 'unenv'
+const prodEnv = env(nodeless, cloudflare, {})
+const devEnv = env({}, {}, {})
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  nitro: {
+    unenv: process.env.NODE_ENV === 'development' ? devEnv : prodEnv,
+  },
   modules: ['nuxt-security']
 })
